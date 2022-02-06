f06effb890c0439961cd6353ded4e9fa1c3e2dd0
Ademola Okerinde <okerinde@selene.beocat.ksu.edu>
2022-02-06 17:49:55 -0600

    added .gitignore

--- codes/run_transformer.sh
+++ codes/run_transformer.sh
@@ -3,4 +3,4 @@ module load Python/3.7.4-GCCcore-8.3.0
 #module load fosscuda
 source ~/virtualenvs/anomaly_sport_video/bin/activate
 export PYTHONDONTWRITEBYTECODE=1
-python /homes/okerinde/projects/anomaly_detection_sport_video/codes/detecting_risky_tackles_in_sport_videos_using_transformer_beocat_v2.py
+python /homes/okerinde/projects/anomaly_detection_sport_video/codes/detecting_risky_tackles_in_sport_videos_using_transformer_beocat_v3.py
