ab38d2dcf915e18989bca5542ffbd5aa6dddd519
Ademola Okerinde <okerinde@eos.beocat.ksu.edu>
2022-02-06 07:30:58 -0600

    beocat code implementation

---
+++ codes/run_transformer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+module load Python/3.7.4-GCCcore-8.3.0
+#module load fosscuda
+source ~/virtualenvs/anomaly_sport_video/bin/activate
+export PYTHONDONTWRITEBYTECODE=1
+python /homes/okerinde/projects/anomaly_detection_sport_video/codes/detecting_risky_tackles_in_sport_videos_using_transformer_beocat.py
